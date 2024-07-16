9d244657ca2eb14eb9dbf5cf6b57ccc1b8a37b67
GAURAV SHARMA <gsharma1154@gmail.com>
2024-07-16 22:59:27 +0530

    without lambda

---
+++ src/main/java/com/telusko/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.telusko.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//                configurer.disable();
+//            }
+//        };
+//        http.csrf(custCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> custHttp=new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//            @Override
+//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+//              registry.anyRequest().authenticated();
+//            }
+//        };
+// http.authorizeHttpRequests(custHttp);
+
+        http.csrf(AbstractHttpConfigurer::disable).
+                authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+}
